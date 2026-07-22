f140a3f60fae50bf61b86b8a0c71562c89e3b3ed
Bedanta004 <mahantybedanta@gmail.com>
2026-07-22 12:36:11 +0530

    adding quizapp

--- QuizNova/quizapp/src/main/java/com/BedantaM/quizapp/dao/QuizDao.java
+++
@@ -1,7 +0,0 @@
-package com.BedantaM.quizapp.dao;
-
-import com.BedantaM.quizapp.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao extends JpaRepository<Quiz, Integer> {
-}
